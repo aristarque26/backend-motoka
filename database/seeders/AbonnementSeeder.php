15fd7c580ee7544469d0bf01adb4aefbe1ca9102
Volonte-77 <volontemuhi@gmail.com>
2026-06-13 17:37:45 +0100

    feat: Populate AbonnementSeeder with initial subscription plans

--- database/seeders/AbonnementSeeder.php
+++ database/seeders/AbonnementSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Abonnement;
 
 class AbonnementSeeder extends Seeder
 {
@@ -12,6 +12,46 @@ class AbonnementSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Abonnement::updateOrCreate(
+            ['slug' => 'starter'],
+            [
+                'nomAgence' => 'Starter',
+                'description' => 'Idéal pour les petites agences locales.',
+                'prix_mensuel' => 49000,
+                'prix_annuel' => 500000,
+                'devise' => 'CDF',
+                'max_succursales' => 3,
+                'max_vehicules' => 5,
+                'max_utilisateurs' => 2,
+            ]
+        );
+
+        Abonnement::updateOrCreate(
+            ['slug' => 'business'],
+            [
+                'nomAgence' => 'Business',
+                'description' => 'Le meilleur choix pour les agences en croissance.',
+                'prix_mensuel' => 129000,
+                'prix_annuel' => 1300000,
+                'devise' => 'CDF',
+                'max_succursales' => 10,
+                'max_vehicules' => 20,
+                'max_utilisateurs' => 10,
+            ]
+        );
+
+        Abonnement::updateOrCreate(
+            ['slug' => 'enterprise'],
+            [
+                'nomAgence' => 'Enterprise',
+                'description' => 'Puissance totale pour les grands réseaux.',
+                'prix_mensuel' => 299000,
+                'prix_annuel' => 3000000,
+                'devise' => 'CDF',
+                'max_succursales' => 999,
+                'max_vehicules' => 999,
+                'max_utilisateurs' => 999,
+            ]
+        );
     }
 }
